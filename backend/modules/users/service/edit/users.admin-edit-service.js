4e49bf13db966d07b9551ab3279f97e399607466
cuongchuck <cuong.buiducmanh@gmail.com>
2025-11-14 12:15:44 +0700

    Add edit user feature for admin

---
+++ backend/modules/users/service/edit/users.admin-edit-service.js
@@ -0,0 +1,24 @@
+const IUserEditService = require('./users.interface-edit');
+const { db } = require('../../../../core/models');
+
+class UserAdminEditService extends IUserEditService {
+  constructor({ userRepository }) {
+    super();
+    this.userRepository = userRepository;
+  }
+
+  async editUser({ id, name, username, email, password }) {
+    const transaction = db.sequelize.transaction();
+    try {
+      const user = await this.userRepository.updateUser({ id, name, username, email, password }, transaction);
+      await transaction.commit();
+      return user;
+    }
+    catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+}
+
+module.exports = UserAdminEditService;
