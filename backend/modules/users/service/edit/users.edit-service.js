e94160a3287aafa7d02d7d9bfe49ee1d2d841e01
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-28 01:17:36 +0700

    Retrieve new public key, Hash new password

--- backend/modules/users/service/edit/users.edit-service.js
+++ backend/modules/users/service/edit/users.edit-service.js
@@ -1,5 +1,8 @@
 import fs from 'fs';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import IUserEditService from './users.interface-edit.js';
 import db from '../../../../core/models/index.js';
@@ -13,10 +16,13 @@ class UserEditService extends IUserEditService {
   async editUser(data) {
     const transaction = await db.sequelize.transaction();
     try {
-      const publicKey = fs.readFileSync('../../../../public.pem');
-      const decodedPayload = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const publicKey = fs.readFileSync(path.resolve(__dirname, '../../../../public_key.pem'), 'utf-8');
+      const decodedPayload = jwt.verify(data.token, publicKey, { algorithms: ['RS256'] });
       data.id = decodedPayload.id;
-      const user = await this.userRepository.updateUser(data, { transaction });
+      data.password = await bcrypt.hash(data.password, 10);
+      const user = await this.userRepository.updateUser(data, transaction);
       await transaction.commit();
       return user;
     }
