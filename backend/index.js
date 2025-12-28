684212179c8bf170b1214025b897c2903145cc8e
Bui Duc Manh Cuong <cuong.buiducmanh@gmail.com>
2025-12-29 00:39:22 +0700

    Update cors credentials

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,8 @@ const app = express();
 
 app.use(cors({
   origin: 'http://localhost:4200',
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
 app.use(express.json());
 app.use(errorHandler);
