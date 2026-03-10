613e412c648e665551f4f92610b886268faa4bff
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-11 00:04:10 +0700

    Use new dynamic config file

--- backend/core/models/index.js
+++ backend/core/models/index.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { Sequelize, DataTypes, Model } from 'sequelize';
 import process from 'process';
 import { fileURLToPath, pathToFileURL } from 'url';
-import configData from '../config/config.json' with { type: 'json' };
+import configData from '../config/db_config.cjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
