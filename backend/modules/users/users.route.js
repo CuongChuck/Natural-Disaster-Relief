6576c24d7f6a6c1a4413fe6c9a5a4376c1c1e65e
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-17 21:47:31 +0700

    Simplify put, delete routes

--- backend/modules/users/users.route.js
+++ backend/modules/users/users.route.js
@@ -9,7 +9,7 @@ const authHandler = container.resolve('authHandler');
 router.get('/user', authHandler.verifyToken, userController.get);
 router.post('/user/register', userController.register);
 router.post('/user/auth', userController.signIn);
-router.put('/user/edit', authHandler.verifyToken, userController.edit);
-router.delete('/user/delete', authHandler.verifyToken, userController.delete);
+router.put('/user', authHandler.verifyToken, userController.edit);
+router.delete('/user', authHandler.verifyToken, userController.delete);
 
 export default router;
