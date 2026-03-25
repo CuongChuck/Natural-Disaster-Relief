b1215e1e949ae0750995af44c26c5ed169591e9f
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-25 12:26:05 +0700

    Add supply services

--- backend/modules/supply/supply.controller.js
+++ backend/modules/supply/supply.controller.js
@@ -16,15 +16,53 @@ class SupplyController {
     }
   };
 
-  create = async (req, res, next) => {
+  getOne = async (req, res, next) => {
     try {
-      const user = req.headers['token'];
-      const data = { user, ...req.body };
-      const result = await this.supplyFacade.create(data);
-      res.status(201).json({
+      const data = { id: req.params.id };
+      const result = await this.supplyFacade.getOne(data);
+      res.status(200).json({
         message: result.message,
         supply: result.supply
       });
+    }
+    catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+
+  getMine = async (req, res, next) => {
+    try {
+      const data = req.body || {};
+      data.userId = req.userId;
+      const result = await this.supplyFacade.getMine(data);
+      res.status(200).json({
+        message: result.message,
+        supplies: result.supplies
+      });
+    }
+    catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+
+  addProof = async (req, res, next) => {
+    try {
+      const data = req.body;
+      data.userId = req.userId;
+      const result = await this.supplyFacade.create(data);
+      res.status(201).json({ message: result.message });
+    }
+    catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+
+  create = async (req, res, next) => {
+    try {
+      const data = req.body;
+      data.userId = req.userId;
+      const result = await this.supplyFacade.create(data);
+      res.status(201).json({ message: result.message });
     }
     catch (err) {
       res.status(500).json({ message: err.message });
