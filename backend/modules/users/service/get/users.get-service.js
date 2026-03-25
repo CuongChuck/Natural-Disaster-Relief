8a9e48cc2bea3368c78613def998479e032f77f2
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-25 12:20:59 +0700

    Remove transaction

--- backend/modules/users/service/get/users.get-service.js
+++ backend/modules/users/service/get/users.get-service.js
@@ -1,21 +1,16 @@
 import IUserGetService from './users.interface-get.js';
 
 class UserGetService extends IUserGetService {
-  constructor({ userRepository, db }) {
+  constructor({ userRepository }) {
     super();
     this.userRepository = userRepository;
-    this.db = db;
   }
 
   async getUser(data) {
-    const transaction = await this.db.sequelize.transaction();
     try {
-      const user = await this.userRepository.getUser(data, transaction);
-      await transaction.commit();
-      return user;
+      return await this.userRepository.getUser(data);
     }
     catch (err) {
-      await transaction.rollback();
       throw err;
     }
   }
