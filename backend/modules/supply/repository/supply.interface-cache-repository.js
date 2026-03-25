40fff2219175b61af40a1286208caea205b34411
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-25 12:24:54 +0700

    Add ISP supply interfaces

---
+++ backend/modules/supply/repository/supply.interface-cache-repository.js
@@ -0,0 +1,9 @@
+export const ISupplyCacheRepository = (Base) => class extends Base {
+  async edit(data) {
+    throw new Error('Method not implemented.');
+  }
+
+  async delete(data) {
+    throw new Error('Method not implemented.');
+  }
+};
---
+++ backend/modules/supply/repository/supply.interface-storage-repository.js
@@ -0,0 +1,5 @@
+export const ISupplyStorageRepository = (Base) => class extends Base {
+  async addProof(data) {
+    throw new Error('Method not implemented.');
+  }
+}
