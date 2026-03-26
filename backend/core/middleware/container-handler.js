1dc248802f580b6583f683d3a8e14cd009cfe602
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-26 23:29:38 +0700

    useRedis can get scope from previous middleware

--- backend/core/middleware/container-handler.js
+++ backend/core/middleware/container-handler.js
@@ -3,20 +3,17 @@ import { asClass } from "awilix";
 import SupplyRedisRepository from "../../modules/supply/repository/supply.redis-repository.js";
 
 export default class ContainerHandler {
-  registerRedis = (scope) => {
-    scope.register({
-      supplyRepository: asClass(SupplyRedisRepository).scoped()
-    });
-  };
-
   useRedis = (req, res, next) => {
     try {
       const container = req.app.get('container');
-      req.scope = container.createScope();
-      this.registerRedis(req.scope);
+      const scope = req.scope || container.createScope();
+      scope.register({
+        supplyRepository: asClass(SupplyRedisRepository).scoped()
+      });
+      req.scope = scope;
       next();
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
-  };
+  }
 };
