f493d63ac9bf7aab210f69c6376526d72ddcc0ef
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-29 12:33:55 +0700

    Register redis-related services

--- backend/core/middleware/container-handler.js
+++ backend/core/middleware/container-handler.js
@@ -1,6 +1,10 @@
 import { asClass } from "awilix";
 
 import SupplyRedisRepository from "../../modules/supply/repository/supply.redis-repository.js";
+import SupplyGetAllUnverified from "../../modules/supply/service/get-all/supply.get-all-unverified.js";
+import SupplyGetOneUnverified from "../../modules/supply/service/get-one/supply.get-one-unverified.js";
+import SupplyGetMineUnverified from "../../modules/supply/service/get-mine/supply.get-mine-unverified.js";
+import SupplyCreateService from "../../modules/supply/service/create/supply.create-service.js";
 
 export default class ContainerHandler {
   useRedis = (req, res, next) => {
@@ -8,7 +12,11 @@ export default class ContainerHandler {
       const container = req.app.get('container');
       const scope = req.scope || container.createScope();
       scope.register({
-        supplyRepository: asClass(SupplyRedisRepository).scoped()
+        supplyRepository: asClass(SupplyRedisRepository).scoped(),
+        supplyGetAllService: asClass(SupplyGetAllUnverified).scoped(),
+        supplyGetOneService: asClass(SupplyGetOneUnverified).scoped(),
+        supplyGetMineService: asClass(SupplyGetMineUnverified).scoped(),
+        supplyCreateService: asClass(SupplyCreateService).scoped()
       });
       req.scope = scope;
       next();
