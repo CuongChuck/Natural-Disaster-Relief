1042a097b42261ab59093144e1e5c4a4a6bb5db2
cuongchuck <cuong.buiducmanh@gmail.com>
2026-05-03 23:59:06 +0700

    Add transaction to many-operation service

--- backend/modules/delivery/service/edit/delivery.add-proof.js
+++ backend/modules/delivery/service/edit/delivery.add-proof.js
@@ -1,20 +1,36 @@
 import IDeliveryEditService from "./delivery.interface-edit.js";
 
 export default class DeliveryAddProof extends IDeliveryEditService {
-  constructor({ deliverySqlRepository, userCheckService, supplyEditService }) {
+  constructor({ deliverySqlRepository, userCheckService, supplyEditService, eventCreateService, db }) {
     super();
     this.deliveryRepository = deliverySqlRepository;
     this.userCheckService = userCheckService;
     this.supplyEditService = supplyEditService;
+    this.eventCreateService = eventCreateService;
+    this.db = db;
   }
 
   edit = async (data) => {
     try {
       await this.userCheckService.checkOperator({ userId: data.userId });
-      await this.deliveryRepository.checkOwner(data);
-      await this.deliveryRepository.addProof(data);
-      const supplyId = await this.deliveryRepository.getSupplyId(data);
-      await this.supplyEditService.edit({ id: supplyId, status: 6 });
+      return await this.db.sequelize.transaction(async () => {
+        await this.deliveryRepository.checkOwner(data);
+        await this.deliveryRepository.addProof(data);
+        const supplyId = await this.deliveryRepository.getSupplyId(data);
+        await this.supplyEditService.edit({ id: supplyId, status: 6 });
+        const delivery = await this.deliveryRepository.getOne({ id: data.id });
+        await this.eventCreateService.create({
+          userId: data.userId,
+          description: null,
+          name: 4,
+          startTime: new Date(),
+          address_line: delivery.address_line,
+          ward: delivery.ward,
+          district: delivery.district,
+          city_province: delivery.city_province,
+          supplies: [supplyId]
+        });
+      });
     } catch (err) {
       throw err;
     }
--- backend/modules/delivery/service/edit/delivery.assign-operator.js
+++ backend/modules/delivery/service/edit/delivery.assign-operator.js
@@ -1,19 +1,22 @@
 import IDeliveryEditService from "./delivery.interface-edit.js";
 
 export default class DeliveryAssignOperator extends IDeliveryEditService {
-  constructor({ deliverySqlRepository, userCheckService, supplyEditService }) {
+  constructor({ deliverySqlRepository, userCheckService, supplyEditService, db }) {
     super();
     this.deliveryRepository = deliverySqlRepository;
     this.userCheckService = userCheckService;
     this.supplyEditService = supplyEditService;
+    this.db = db;
   }
 
   edit = async (data) => {
     try {
       await this.userCheckService.checkOperator({ userId: data.userId });
-      await this.deliveryRepository.assignOperator(data);
-      const supplyId = await this.deliveryRepository.getSupplyId(data);
-      await this.supplyEditService.edit({ id: supplyId, status: 5 });
+      return await this.db.sequelize.transaction(async () => {
+        await this.deliveryRepository.assignOperator(data);
+        const supplyId = await this.deliveryRepository.getSupplyId(data);
+        await this.supplyEditService.edit({ id: supplyId, status: 5 });
+      });
     } catch (err) {
       throw err;
     }
